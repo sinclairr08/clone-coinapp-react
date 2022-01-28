4a4e3f613eeb107d178c66b16489a23d11850a25
Junhyuk Lee <ljhjoon00@gmail.com>
2022-01-28 11:32:18 +0900

    fix : Fix a problem on chart date representation

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -35,7 +35,7 @@ function Chart({ coinId }: ChartProps) {
             {
               name: "ohlc",
               data: data?.map((price) => ({
-                x: new Date(price.time_close),
+                x: new Date(price.time_close.slice(0, 10)),
                 y: [
                   price.open.toFixed(2),
                   price.high.toFixed(2),
@@ -85,58 +85,3 @@ function Chart({ coinId }: ChartProps) {
 }
 
 export default Chart;
-/*
-<div>
-      {isLoading ? (
-        "Loading chart..."
-      ) : (
-        <ApexChart
-          type="line"
-          series={[
-            {
-              name: "Price",
-              data: data?.map((price) => price.close),
-            },
-          ]}
-          options={{
-            theme: {
-              mode: isDark ? "dark" : "light",
-            },
-            chart: {
-              height: 300,
-              width: 500,
-              toolbar: {
-                show: false,
-              },
-              background: "transparent",
-            },
-            grid: {
-              show: false,
-            },
-            stroke: {
-              curve: "smooth",
-              width: 4,
-            },
-            yaxis: { show: false },
-            xaxis: {
-              labels: { show: false },
-              axisTicks: { show: false },
-              axisBorder: { show: false },
-              type: "datetime",
-              categories: data?.map((price) => price.time_close),
-            },
-            fill: {
-              type: "gradient",
-              gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
-            },
-            colors: ["#0fbcf9"],
-            tooltip: {
-              y: {
-                formatter: (value) => `$ ${value.toFixed(2)}`,
-              },
-            },
-          }}
-        />
-      )}
-    </div>
-*/
